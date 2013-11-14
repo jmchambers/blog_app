1e75167d340c8b15e2c9a349f5e68e27168c078c
jmchambers <j.chambers@gmx.net>
2013-11-14 14:10:51 +0000

    created rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoolApp::Application.config.secret_token = '74fb601fa287997f08390a65aafd457c767a1300d5d8556e79ed1d77fd6495cb25cc18bef4368168e0eccfbe0a856bf2924d2b502c18b534e5cd689213177df1'
